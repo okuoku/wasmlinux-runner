c75eac3e5132223c2e00272ad9649a17feea9ded
okuoku <mjt@cltn.org>
2024-02-25 20:08:03 +0900

    emscripten: Add HTML

---
+++ emframe/_serve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from http import server # Python 3
+
+class TheHandler(server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+        server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+
+
+server.test(HandlerClass=TheHandler, port=3000)
